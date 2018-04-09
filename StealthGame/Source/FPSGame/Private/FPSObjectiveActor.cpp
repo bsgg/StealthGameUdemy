43eaf55a79454b9854edc9633847019c82d91183
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2018-04-09 19:53:42 +0100

    Include UI

--- StealthGame/Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ StealthGame/Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -4,13 +4,12 @@
 #include "Components/StaticMeshComponent.h"
 #include "Components/SphereComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "FPSCharacter.h"
 
 
 // Sets default values
 AFPSObjectiveActor::AFPSObjectiveActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
@@ -34,12 +33,6 @@ void AFPSObjectiveActor::BeginPlay()
 	//PlayEffects();
 }
 
-// Called every frame
-void AFPSObjectiveActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
 void AFPSObjectiveActor::PlayEffects()
 {
@@ -48,9 +41,16 @@ void AFPSObjectiveActor::PlayEffects()
 
 void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor)
 {
-	Super::NotifyActorBeginOverlap(OtherActor);
+	Super::NotifyActorBeginOverlap(OtherActor); 
 
 	PlayEffects();
+
+	AFPSCharacter* myCharacter = Cast<AFPSCharacter>(OtherActor);
+	if (myCharacter != nullptr)
+	{
+		myCharacter->bIsCarryingObjective = true;
+		Destroy();
+	}
 
 }
 
