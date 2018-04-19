aef10d15b3474d07012cbf6653cda107370893d2
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2018-04-19 08:28:51 +0100

    Include noise in projectile

--- StealthGame/Source/FPSGame/Private/FPSCharacter.cpp
+++ StealthGame/Source/FPSGame/Private/FPSCharacter.cpp
@@ -52,7 +52,7 @@ void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 void AFPSCharacter::Fire()
 {
 	// try and fire a projectile
-	if (ProjectileClass)
+	if (ProjectileClass) 
 	{
 		FVector MuzzleLocation = GunMeshComponent->GetSocketLocation("Muzzle");
 		FRotator MuzzleRotation = GunMeshComponent->GetSocketRotation("Muzzle");
@@ -60,6 +60,7 @@ void AFPSCharacter::Fire()
 		//Set Spawn Collision Handling Override
 		FActorSpawnParameters ActorSpawnParams;
 		ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+		ActorSpawnParams.Instigator = this; // Set instigator as this character so in FPSProjectile this variable won't be null
 
 		// spawn the projectile at the muzzle
 		GetWorld()->SpawnActor<AFPSProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, ActorSpawnParams);
